6cc2e0a71355004e9b0e0c0ea2680f3144286b81
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-03-02 20:48:08 +0200

    add auth

---
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends DefaultController
+{
+    public function index()
+    {
+
+    }
+}
---
+++ app/Http/Controllers/Admin/UploadController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Admin\DefaultController;
+use Illuminate\Http\Request;
+
+class UploadController extends DefaultController
+{
+
+}
