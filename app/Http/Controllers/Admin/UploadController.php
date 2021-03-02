31020853d499ee093d1f9045f743070c271e04a2
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-03-03 01:11:42 +0200

    fx

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -8,4 +8,13 @@ use Illuminate\Http\Request;
 class UploadController extends DefaultController
 {
 
+    public function index()
+    {
+        return view('admin.upload.index');
+    }
+
+    public function uploadChunk(Request $request)
+    {
+        dd($request);
+    }
 }
