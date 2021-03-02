7c2fac4100bd984e2dfeba9ad85e6c3250edc384
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-03-02 20:25:00 +0200

    add models

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function videos()
+    {
+        return $this->hasMany(Video::class);
+    }
 }
