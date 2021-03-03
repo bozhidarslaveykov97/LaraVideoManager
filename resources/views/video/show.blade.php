4371a5855f2c0e1ecd4f346504c1493e49ab23ce
Bozhidar Slaveykov <selfworksbg@gmail.com>
2021-03-03 21:36:17 +0200

    fix

--- resources/views/video/show.blade.php
+++ resources/views/video/show.blade.php
@@ -1,6 +1,6 @@
 
 
-<video id="js-video-player" width="620" height="640" preload="metadata" autoplay controls>
+<video id="js-video-player" width="610" height="610" preload="metadata" autoplay controls>
     <source src="{{url(route('video.stream', $video->id))}}">
     Your browser does not support the video tag.
 </video>
